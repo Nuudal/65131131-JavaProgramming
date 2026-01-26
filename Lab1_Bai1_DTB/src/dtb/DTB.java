b96f2040dc9f2ab6dc3e22660581c46ad34d0f56
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-01-26 10:02:54 +0700

    Update DTB.java

--- Lab1_Bai1_DTB/src/dtb/DTB.java
+++ Lab1_Bai1_DTB/src/dtb/DTB.java
@@ -14,8 +14,7 @@ public class DTB {
         }
         while (b<0 || b>10);
         
-        System.out.println("Ho va ten: " + a);
-        System.out.println("Diem trung binh: " + b);
+        System.out.printf("%s: %.2f điểm", a, b);
 
         scanner.close();
     }
