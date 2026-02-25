b14fb31fc8e28d4cc3127f33c3053b96d99e9ee3
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-02-25 20:15:05 +0700

    Lab 3 Bai 2

---
+++ Lab3_Bai2_BangCuuChuong/src/bangcuuchuong/BangCuuChuong.java
@@ -0,0 +1,16 @@
+package bangcuuchuong;
+
+public class BangCuuChuong {
+
+	public static void main(String[] args) {
+		for (int j=1;j<=10;j++)
+		{
+			for (int i=1;i<=10;i++)
+			{
+				System.out.printf("%d x %d = %d\n",i,j,i*j);
+			}
+			System.out.println();
+		}
+		
+	}
+}
